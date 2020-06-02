a312f69480579c3695139e4e9de1daa070bb7f99
Roger Campbell II <rc1336@gmail.com>
2020-06-02 02:30:01 -0400

    Added netlify form info.

--- src/components/Main.js
+++ src/components/Main.js
@@ -109,7 +109,9 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Call on Stevi</h2>
-          <form method="post" action="#">
+          <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact" />
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
